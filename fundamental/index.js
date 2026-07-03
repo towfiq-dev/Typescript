63f9b172a353ca515503920b8d3f7d52a208c876
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-07-03 09:35:14 +0600

    Initial commit on Fri, Jul  3, 2026  9:35:14 AM

---
+++ fundamental/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let userName = 'Towfiqul Islam';
+console.log(userName);
+function addNumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumbers(20, 30);
---
+++ fundamental/index.ts
@@ -0,0 +1,8 @@
+let userName = 'Towfiqul Islam'
+console.log(userName);
+
+function addNumbers(num1: number, num2: number) {
+  console.log(num1 + num2);
+}
+
+  addNumbers(20, 30)
