d5a17b3cc975c3953273920c49efd478f6189297
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-07-03 10:51:31 +0600

    Auto-update on Fri, Jul  3, 2026 10:51:31 AM

---
+++ user-defined-type/array-type.js
@@ -0,0 +1,8 @@
+"use strict";
+let userNames;
+userNames = ["anis", "rabu", "pinky"];
+userNames.sort();
+console.log(userNames);
+let multipleType;
+multipleType = ['rohim', 'korim', 12, 15];
+console.log(multipleType[2]);
---
+++ user-defined-type/array-type.ts
@@ -0,0 +1,12 @@
+let userNames: string[]
+
+userNames = ["anis", "rabu", "pinky"]
+
+userNames.sort()
+console.log(userNames);
+
+let multipleType: (string | number)[]
+
+multipleType = ['rohim', 'korim', 12, 15]
+console.log(multipleType[2]);
+
