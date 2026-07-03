b55b9aa58878a7731be12ee0f1d4b3cc1cd769c0
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-07-03 11:17:27 +0600

    Auto-update on Fri, Jul  3, 2026 11:17:27 AM

---
+++ user-defined-type/tuple-type.js
@@ -0,0 +1,5 @@
+"use strict";
+let user;
+user = [101, 'anis'];
+user.push(102, "riya");
+console.log(user);
---
+++ user-defined-type/tuple-type.ts
@@ -0,0 +1,6 @@
+let user: [number, string] 
+
+
+user = [101, 'anis']
+user.push(102, "riya")
+console.log(user);
